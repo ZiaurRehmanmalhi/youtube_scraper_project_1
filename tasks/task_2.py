137be4daacc00c064879e1c9aa50a89e4bfa35bd
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-04-11 17:37:26 +0500

    modify code

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,43 +1,17 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
 import pandas as pd
+from utils import scroll_to_page_end, extract_videos_data
 
 url = "https://www.youtube.com/@zusmani78/videos"
 driver = webdriver.Chrome(executable_path="chromedriver")
 driver.get(url)
-last_height = driver.execute_script("return document.documentElement.scrollHeight")
-
-while True:
-    driver.execute_script("window.scrollTo(0, arguments[0]);", last_height)
-    time.sleep(1.5)
-    new_height = driver.execute_script("return document.documentElement.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+scroll_to_page_end(driver)       # utils function_1 call
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-all_content = soup.find_all('ytd-rich-item-renderer')
+data = extract_videos_data(soup)      # utils function_2 call
 
-data = []
-for videos in all_content:
-    thumbnail = videos.find('img', {"style": "background-color: transparent;"}).get('src')
-    title = videos.find('yt-formatted-string', {"id": "video-title"}).text
-    views = videos.find('span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}).text
-    upload_time = videos.find('span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}).get('before')
-    video_duration = videos.find(
-        'span', {"class": "style-scope ytd-thumbnail-overlay-time-status-renderer"}
-    ).text.strip()
- 
-    all_data = {
-        "Title": title,
-        "Views": views,
-        "Video Duration": video_duration,
-        "Thumbnail Link": thumbnail
-    }
-    data.append(all_data)
-
-df = pd.DataFrame(data)
-df.to_csv('youtube_data.csv', index=False)
+dataframe = pd.DataFrame(data)
+dataframe.to_csv('zusmani78.csv', index=False)
 print("Data saved to csv file")
---
+++ tasks/utils.py
@@ -0,0 +1,36 @@
+import time
+
+
+def scroll_to_page_end(driver):
+    last_height = driver.execute_script("return document.documentElement.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, arguments[0]);", last_height)
+        time.sleep(1.5)
+        new_height = driver.execute_script("return document.documentElement.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+def extract_videos_data(soup):
+    video_elements = soup.find_all('ytd-rich-item-renderer')
+    videos_data = []
+    for videos in video_elements:
+        thumbnail = videos.find('img', {"style": "background-color: transparent;"}).get('src')
+        title = videos.find('yt-formatted-string', {"id": "video-title"}).text
+        views = videos.find('span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}).text
+        uploaded_at = videos.find(
+            'span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}
+        ).get('before')
+        video_duration = videos.find(
+            'span', {"class": "style-scope ytd-thumbnail-overlay-time-status-renderer"}
+        ).text.strip()
+
+        all_video_data = {
+            "Title": title,
+            "Views": views,
+            "Video Duration": video_duration,
+            "Thumbnail Link": thumbnail
+        }
+        videos_data.append(all_video_data)
+    return videos_data
